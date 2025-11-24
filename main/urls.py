b8ebfd475889ac4a55764098cfbf4df7b8205618
Fernandinho-8 <nandofilhodedeus786@gmail.com>
2025-11-24 11:43:58 -0300

    criei urls e fiz novos defs
    fiz coisas em views,url, teste.html

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,12 @@
+from os import name
 from django.urls import path
+from main import views
 from main.views import TaskList
 
 urlpatterns = [
     path("", TaskList.as_view(), name='task_list'), 
+    path('funcao/', views.task_list, name='task_list'),
+    path('task_concluido', views.task_concluido, name='task_concluido'),
+    path('task_nao_concluido', views.task_nao_concluido, name='task_nao_concluido')
+
 ]
--- main/views.py
+++ main/views.py
@@ -14,3 +14,28 @@ class TaskList(ListView):
     context_object_name = 'tarefas' # como eu defino como eu quero ver os dados sendo mostrados no frontend
 
 #views---url---template
+
+
+def task_list(request):
+    tarefas = Task.objects.all()
+    context = {
+        "tarefas":tarefas,
+        "titulo_pagina": 'Minhas Tarefas'
+    }
+    return render(request, 'tasks/task_list.html', context)
+
+def task_concluido(request):
+    tarefas = Task.objects.filter(concluida=1)
+    context = {
+        "tarefas":tarefas,
+        "titulo_pagina": 'Minhas Tarefas Concluidas',
+    }
+    return render(request, 'tasks/teste.html', context)
+
+def task_nao_concluido(request):
+    tarefas = Task.objects.filter(concluida=0)
+    context = {
+        "tarefas":tarefas,
+        "titulo_pagina": 'Minhas Tarefas nao concluidas',
+    }
+    return render(request, 'tasks/teste.html', context)
